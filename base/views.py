ec658e1e44a50dcd49795e5bc7a1aec7ae7f9ba2
Monu Neolia <neoliamonu@gmail.com>
2022-06-13 07:47:10 +0530

    "add login & logout"

--- base/views.py
+++ base/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Task
 
+from django.contrib.auth.views import LoginView
+
+
+class CustomLoginView(LoginView):
+    template_name = '../templates/base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
 
 class TaskList(ListView):
     model = Task
@@ -14,3 +26,21 @@ class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
     template_name = '../templates/base/task.html'
+
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
